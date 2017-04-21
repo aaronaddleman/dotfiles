b23431bf2e0024237596d79427ad66163def6eaf
Aaron Addleman <aaronaddleman@gmail.com>
2017-04-20 23:21:12 -0700

    fixed aliases

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -2,9 +2,9 @@ alias reload='source ~/.bash_profile && echo "sourced ~/.bash_profile"'
 alias redot='cd ~/.dotfiles && gpp && rake install; cd -'
 
 # remember..
-vaultssh?="echo 'vaultssh .blahlbah'"
-awssg?="echo 'aws ec2 describe-securitygroups' ..."
-awsami?="echo 'aws ec2 descibe-images --filter \"Name=name,Values=thingYourLookingFor\"'"
+alias vaultssh?="echo 'vaultssh .blahlbah'"
+alias awssg?="echo 'aws ec2 describe-securitygroups' ..."
+alias awsami?="echo 'aws ec2 descibe-images --filter \"Name=name,Values=thingYourLookingFor\"'"
 
 # appearance
 alias grep='grep --color=auto'
