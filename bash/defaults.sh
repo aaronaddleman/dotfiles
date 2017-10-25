70d3294a414304d99f90ac627595996f276e79a0
Aaron Addleman <aaron@hiigara.local>
2017-10-24 21:27:31 -0700

    cleanup of dotfiles
    - moved dotfiles into main dir
    - moved bash related files into bash/
    - updated bash/defaults.sh to set some sane defaults
    - removed main/defaults.sh
    - added dircolors support

---
+++ bash/defaults.sh
@@ -0,0 +1,14 @@
+# If not running interactively, don't do anything!
+[[ $- != *i* ]] && return
+
+# Enable history appending instead of overwriting.
+shopt -s histappend
+
+# enable dircolors
+#
+# to use this, you must install coreutils
+if hash gls 2>/dev/null; then
+  #eval $(gdircolors $SCRIPTS_DIR/dircolors/dircolors.ansi-dark)
+  export set LSCOLORS="gxfxbEaEBxxEhEhBaDaCaD"
+  alias ls='gls --color=auto'
+fi
--- bash_profile.sh
+++ bash_profile.sh
@@ -1,21 +1,33 @@
 # shellcheck source=/dev/null
 [ -s ~/.profile ] && . ~/.profile
 
-
-# load defaults
-SOURCE="${BASH_SOURCE[0]}"
-MYBASHDIR=$(dirname "$SOURCE")
-
-source $MYBASHDIR/main/defaults.sh
-
-source $MYBASHDIR/bash/path.sh
-source $MYBASHDIR/bash/env.sh
-source $MYBASHDIR/bash/completion.sh
-source $MYBASHDIR/bash/aliases.sh
-source $MYBASHDIR/bash/functions.sh
-source $MYBASHDIR/bash/prompt.sh
-
-if [ -d "$HOME/.bash.d" ]; then
-  source $HOME/.bash.d/*sh
+# find our script_path
+pushd . > /dev/null
+SCRIPT_PATH="${BASH_SOURCE[0]}";
+if ([ -h "${SCRIPT_PATH}" ]) then
+  while([ -h "${SCRIPT_PATH}" ]) do cd `dirname "$SCRIPT_PATH"`; SCRIPT_PATH=`readlink "${SCRIPT_PATH}"`; done
 fi
+cd `dirname ${SCRIPT_PATH}` > /dev/null
+SCRIPT_PATH=`pwd`;
+popd  > /dev/null
+
+# load bash scripts
+source $SCRIPT_PATH/bash/defaults.sh
+source $SCRIPT_PATH/bash/path.sh
+source $SCRIPT_PATH/bash/env.sh
+source $SCRIPT_PATH/bash/completion.sh
+source $SCRIPT_PATH/bash/aliases.sh
+source $SCRIPT_PATH/bash/functions.sh
+source $SCRIPT_PATH/bash/prompt.sh
+
+# if you have any extra stuff you want to load
+for f in $HOME/.bash.d/*; do
+    ## Check if the glob gets expanded to existing files.
+    ## If not, f here will be exactly the pattern above
+    ## and the exists test will evaluate to false.
+    [ -e "$f" ] && source $HOME/.bash.d/*
+
+    ## This is all we needed to know, so we can break after the first iteration
+    break
+done
 
