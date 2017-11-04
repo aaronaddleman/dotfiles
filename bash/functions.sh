a7452aa3ee15c00daeb59696b5346c0335bf206c
Aaron Addleman <Aaron Addleman aaron.addleman@autodesk.com>
2017-11-04 14:55:18 -0700

    added some repeated commands to fuctions file

--- bash/functions.sh
+++ bash/functions.sh
@@ -1,3 +1,35 @@
 # functions
 
+# scan all dirs for git changes
+function gscan() {
+  for d in */
+  do
+    pwd=$(PWD)
+    cd $d
+    echo "==$d=="
+    git status --short
+    cd $pwd
+  done
+  cd ~/src
+}
+
+# stop all docker conatiners
+function docker_stop_containers() {
+  docker stop $(docker ps -a -q)
+}
+
+# rm all docker containers
+function docker_rm_containers() {
+  docker rm $(docker ps -a -q)
+}
+
+# rm all docker images
+function docker_rm_images() {
+  docker rmi $(docker images -a -q)
+}
+
+function docker_rm_images_forced() {
+  docker rmi $(docker images -a -q) --force
+}
+
 # remove ssh host from known hosts file
