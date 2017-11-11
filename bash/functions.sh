734a9117d26eb10e699f8384af4910ced301f7a8
Aaron Addleman <Aaron Addleman aaronaddleman@gmail.com>
2017-11-10 20:59:59 -0800

    added function fop
    this function is for fetching passwords from 1Password

--- bash/functions.sh
+++ bash/functions.sh
@@ -33,3 +33,10 @@ function docker_rm_images_forced() {
 }
 
 # remove ssh host from known hosts file
+
+# fetch password from 1password
+function fop() {
+  VAULT=$1
+  NAME=$2
+  op get item --vault=$VAULT "$NAME" | jq -r '.details.fields[] | select(.name | contains("password")) | .value'
+}
