9336571d13670368aa612b1bb19c5ef30dfc9bca
Aaron Addleman <Aaron Addleman aaronaddleman@gmail.com>
2018-01-29 11:32:03 -0800

    modify rvm to allow toggle and logging

--- bash/rvm.sh
+++ bash/rvm.sh
@@ -1,6 +1,11 @@
 debug "2" "ENABLE_RVM=${ENABLE_RVM}"
 if [ ${ENABLE_RVM} = "true" ]; then 
-  # Load RVM into a shell session *as a function*
-  [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
+    if [ -d "$HOME/.rvm" ]; then
+        debug "3" "loading rvm"
+        # Load RVM into a shell session *as a function*
+        [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
+    else
+        debug "3" "rvm dir missing: $HOME/.rvm"
+    fi
 fi
 
